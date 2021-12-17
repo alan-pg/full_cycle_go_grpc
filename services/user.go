c333e0cc3603aba70b18f0bee886fb83705bef34
Alan Goncalves <alan92goncalves@gmail.com>
2021-12-17 20:07:31 -0300

    implementando server stream

--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/alan-pg/fc2-grpc/pb/pb"
 )
@@ -24,4 +25,43 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send((&pb.UserResultStream{
+		Status: "init",
+		User:   &pb.User{},
+	}))
+
+	time.Sleep((time.Second * 3))
+
+	stream.Send((&pb.UserResultStream{
+		Status: "inserting",
+		User:   &pb.User{},
+	}))
+
+	time.Sleep((time.Second * 3))
+
+	stream.Send((&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	}))
+
+	time.Sleep((time.Second * 3))
+
+	stream.Send((&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	}))
+
+	return nil
+
 }
